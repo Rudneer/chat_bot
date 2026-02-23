3aa7cd36a9daf1d0cfb7421acf4af4b1d5887d73
unknown <neer8669@gmail.com>
2026-02-23 11:12:17 +0530

    allow all host

--- chatbot_ui/chatbot_ui/settings.py
+++ chatbot_ui/chatbot_ui/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-6mr0#)7dtx^l+d0$pvl9^jt691%b@@cg#jg4*ew^0y5cy@1(*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
